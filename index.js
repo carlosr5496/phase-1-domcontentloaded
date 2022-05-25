37e4631fab8a3f94257b0939207e93660d206b71
Ryan Carlos <ryancarlos925@gmail.com>
2022-05-24 21:45:12 -0700

    commit tree d248e9350dfbd3a3082f4a846b370b57c7e2b267

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('The DOM has loaded');
+    // let p = document.querySelector('p')
+    let txt = document.getElementById('text')
+    txt.textContent = 'This is really cool!'
+});
+
